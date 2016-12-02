da414b3885dc1b0316306145579e0b1e2267d840
Keyen331 <banayos.kay@gmail.com>
2016-12-02 09:15:42 -1000

    update

--- test_review.js
+++ test_review.js
@@ -1,45 +1,46 @@
-// Kay Hian Calpito, November 23, 2016, Javascript test review//
+/* Kay Hian Calpito, November 23, 2016, Javascript test review*/
 var knight = 'Gwain';
 var maiden = 'Guinevere';
 var monster = 'Dragon';
 
-var weaponChest = ['Mace', 'Sorcerer stone', 'Excalibur']
+var weaponChest = ['Mace', 'Sorcerer stone', 'Excalibur'];
 
 var hero = {
-  'wizard' : 'Merlin',
-  'weapon' : 'Mace',
-  'power' : 50
+  wizard : 'Merlin',
+  weapon : weaponChest[0],
+  power : 50
 }
 
 function swordInTheStone(worthy){
-  if (worthy = 'Arthur') {
-    console.log("We have found our King" + " " + worthy)
+  if (worthy == 'Arthur') {
+    console.log("We have found our King " + worthy)
   }
   else {
-    console.log("We must keep searching, for" + worthy + "is unworthy.")
+    console.log("We must keep searching, for " + worthy + " is unworthy.")
   }
 }
-swordInTheStone('Gwain')
+swordInTheStone(knight);
 
 function trueLove(king, queen){
-  if (queen = 'Guinevere' && king = 'Arthur') {
-    console.log("On this day, our" + king + "and" + queen + "shall rule happily ever after")
+  if (queen == 'Guinevere' && king == 'Arthur') {
+    console.log("On this day, our " + king + " and " + queen + " shall rule happily ever after ");
   }
   else {
-    console.log("We must keep searching, for true must reign over Camelot.")
+    console.log("We must keep searching, for true must reign over Camelot.");
   }
 }
-trueLove('Gwain', 'Guinevere')
+trueLove(knight, maiden);
 
 function epicBattle(wizard, enemy, power){
   if (power <= 15) {
-    console.log("Our" + wizard + "has fallen".)
+    console.log("Our " + wizard + " has fallen.");
   }
-  else if (power >= 16 && <= 40) {
-    console.log(wizard + "has almost slain" + enemy)
+  else if (power >= 16 && power <= 40) {
+    console.log(wizard + " has almost slain the " + enemy);
   }
   else {
-    console.log("Our" + wizard + "has slain the" + enemy + "!")
+    console.log("Our " + wizard + " has slain the " + enemy + "!");
   }
 }
-epicBattle('Merlin', 'Dragon', 50)
+epicBattle(hero.wizard, monster, hero.power);
+
